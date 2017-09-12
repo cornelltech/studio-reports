64338396153becbc15a339831301a5aa46e5b396
Rachel Sobel <rachel.sobel@cornell.edu>
2017-09-12 19:31:18 -0400

    complete the vvv ugly template

--- get_reports.py
+++ get_reports.py
@@ -34,23 +34,40 @@ TEAM_MEMBER_KEYS = ['name', 'email']
 ASSETS_KEYS = ['url', 'title']
 
 class Team:
-    def __init__(self, doc):
+    def __init__(self, doc, repo):
         self.roster = build_team(doc['team']['roster'])
         self.how_might_we = doc['how_might_we']
         self.product_narrative = doc['product_narrative']
-        self.company = doc['company']
-        self.assets = doc['assets']
+        self.company = doc['company']['name']
+        self.company_logo = doc['company']['logo']
+        self.assets = build_assets(doc['assets'])
+        self.team_photo_file = get_photo_name(repo, doc['team']['picture'])
 
     class Teammate:
         def __init__(self, name, email):
             self.name = name
             self.email = email
 
+    class Asset:
+        def __init__(self, title, url):
+            self.title = title
+            self.url = url
+
 def build_team(roster):
     team = []
     for person in roster:
         team.append(Team.Teammate(person['name'], person['email']))
     return team
+
+def build_assets(assets):
+    list_of_assets = []
+    for asset in assets:
+        list_of_assets.append(Team.Asset(asset['title'], asset['url']))
+    return list_of_assets
+
+def get_photo_name(repo, img_name):
+    team_img_file = repo.name + '-' + img_name
+    return team_img_file
 
 def get_teams():
     teams = []
@@ -63,13 +80,13 @@ def get_teams():
         doc = yaml.load(yaml_file.decoded_content)
         save_team_picture(repo, doc['team']['picture'])
         # teams.append(doc)
-        teams.append(Team(doc))
+        teams.append(Team(doc, repo))
     return teams
 
 def save_team_picture(repo, img_name):
     url = 'https://raw.githubusercontent.com/' + repo.full_name + '/master/' + img_name
     response = requests.get(url, stream=True, auth=(GITHUB_USER, GITHUB_PASSWORD))
-    team_img_file = repo.name + '-' + img_name
+    team_img_file = get_photo_name(repo, img_name)
     with open(team_img_file, 'wb') as outfile:
         shutil.copyfileobj(response.raw, outfile)
 

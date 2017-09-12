dba05bec8e23481ba65cc6f735363301b60a3809
Rachel Sobel <rachel.sobel@cornell.edu>
2017-09-12 16:53:04 -0400

    create map of teams:[students]

--- create_teams.py
+++ create_teams.py
@@ -15,6 +15,9 @@ except Exception as e:
 
 GITHUB_ACCESS_TOKEN = os.environ.get('GITHUB_ACCESS_TOKEN', None)
 
+STUDENTS_TO_TEAMS = "teams2students.txt"
+
+
 TEAMS_FILE = "team-repos"
 ORG_NAME = "ct-product-challenge-2017"
 STUDIO_TEAM_ID = 2477099
@@ -33,8 +36,31 @@ def create_team_repo(g, repo_name, student_team):
     studio_team.add_to_repos(g.get_organization(ORG_NAME).get_repo(repo_name))
     return repo
 
+def create_teams_map():
+    students = {}
+    with open(STUDENTS_TO_TEAMS) as f:
+        lines = [line.strip() for line in f.readlines()]
+        for line in lines:
+            line = line.split('\t')
+            if len(line) == 2:
+                students[line[1]] = line[0]
+            elif line[0]:
+                print line, 'team member missing github username'
+    teams = {}
+    for student in students.keys():
+        team = students[student]
+        if team in teams:
+            teams[team].append(student)
+        else:
+            teams[team] = [student]
+    return teams
+
 if __name__ == '__main__':
-    g = github.Github(GITHUB_ACCESS_TOKEN)
-    students = ['elanid']
-    t = create_student_team(g, 'cool-story-bro', students)
-    repo = create_team_repo(g, 'cool-story-bro', t)
+    # g = github.Github(GITHUB_ACCESS_TOKEN)
+    # students = ['elanid', 'sahuguet']
+    # t = create_student_team(g, 'example-product-team', students)
+    # repo = create_team_repo(g, 'example-product-team', t)
+
+    teams = create_teams_map()
+    for team in teams.keys():
+        print team

722c4d5ed9a00229bd033883a0a72c882e64081b
Rachel Sobel <rachel.sobel@cornell.edu>
2017-09-15 00:10:47 -0400

    fix so that non-ascii characters don't break the site

--- update_website.py
+++ update_website.py
@@ -1,7 +1,10 @@
 import get_reports
+import unicodedata
 
 OUTPUT_PATH = get_reports.OUTPUT_DIR + get_reports.OUTPUT_FILE
 
 if __name__ == '__main__':
     with open(OUTPUT_PATH, 'w') as outfile:
-        outfile.write(get_reports.create_index_page())
+        outfile.write(unicodedata.normalize('NFKD',
+                    get_reports.create_index_page()).encode('ascii','ignore')
+)

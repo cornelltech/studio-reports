bf830695e7985f062a0376e9ae3bc4978600e8c7
Rachel Sobel <rachel.sobel@cornell.edu>
2017-09-21 15:15:34 -0400

    adding in file processing

--- build_buildboard.py
+++ build_buildboard.py
@@ -20,6 +20,7 @@ except Exception as e:
 GITHUB_ACCESS_TOKEN = os.environ.get('GITHUB_ACCESS_TOKEN', None)
 ORG_NAME = "ct-product-challenge-2017"
 YAML_FILE_NAME = "report.yaml"
+TEAMS_FILE_NAME = "teams"
 
 OUTPUT_DIR_NAME = "output"
 YAML_DIR_NAME = "yaml"
@@ -35,10 +36,11 @@ def get_photo_url(repo_name, img_name):
     return 'https://raw.githubusercontent.com/%s/%s/master/%s' % (ORG_NAME, repo_name, img_name)
 
 def save_photo_path(output_dir_name, repo_name, img_name):
-    save_photo_as = os.path.join(OUTPUT_DIR_NAME, output_dir_name, "%s-%s" % (repo_name, img_name))
-    return save_photo_as
+    return os.path.join(OUTPUT_DIR_NAME, output_dir_name,
+                        "%s-%s" % (repo_name, img_name))
 
 def save_photo(url, output_path):
+    print 'saving %s' % os.path.basename(output_path)
     access_token = 'token %s' % GITHUB_ACCESS_TOKEN
     response = requests.get(url, stream=True, headers={'Authorization': access_token})
     with open(output_path, 'wb') as outfile:
@@ -89,9 +91,27 @@ def save_team_files(teams, yaml_dir):
         except github.GithubException, e:
             print "There's a problem with that repository's yaml file:", str(e)
 
-def create_output_directories():
-    pwd = os.path.dirname(os.path.realpath(__file__))
-    output_dir = os.path.join(pwd, OUTPUT_DIR_NAME)
+def save_team_file(team, yaml_dir, g):
+    print 'getting yaml file for %s...' % team
+    try:
+        repo_name = "%s/%s" % (ORG_NAME, team)
+        repo = g.get_repo(repo_name)
+        team_yaml_file = os.path.join(yaml_dir, "%s.yaml" % team)
+        with open(team_yaml_file, 'w') as outfile:
+            yaml_file = repo.get_file_contents(YAML_FILE_NAME)
+            outfile.write(yaml_file.decoded_content)
+    except github.GithubException, e:
+        print "There's a problem with that repository's yaml file:", str(e)
+
+def get_teams(teams_file):
+    with open(teams_file) as tf:
+        team_metadata = [team.strip() for team in tf.readlines()]
+    team_names = [team.split("\t")[3] for team in team_metadata]
+    return (team_names, team_metadata)
+
+
+def create_output_directories(target_directory):
+    output_dir = os.path.join(target_directory, OUTPUT_DIR_NAME)
     if not os.path.exists(output_dir):
         print 'creating new directory:', output_dir
         os.makedirs(output_dir)
@@ -110,9 +130,15 @@ def create_output_directories():
     return (output_dir, yaml_dir, team_photos_dir, company_logos_dir)
 
 if __name__ == '__main__':
-    (output_dir, yaml_dir, team_photos_dir, company_logos_dir) = create_output_directories()
-    save_team_files(['example-product-team'], yaml_dir)
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    (output_dir, yaml_dir, team_photos_dir, company_logos_dir) = create_output_directories(pwd)
+    teams_file = os.path.join(pwd, TEAMS_FILE_NAME)
+    (team_names, team_metadata) = get_teams(teams_file)
+    # g = github.Github(GITHUB_ACCESS_TOKEN)
+    # for team in team_names:
+    #     save_team_file(team, yaml_dir, g)
     yaml_files = os.listdir(yaml_dir)
     for yaml_file in yaml_files:
         yaml_file = os.path.join(yaml_dir, yaml_file)
         process_yaml_file(yaml_file)
+    # save_team_files(['example-product-team'], yaml_dir)

0087c878508a7d9692ecffb958508fb4df4eafe4
Rachel Sobel <rachel.sobel@cornell.edu>
2017-09-21 17:43:57 -0400

    restore update_website to previous state

--- update_website.py
+++ update_website.py
@@ -4,23 +4,15 @@ import get_reports
 import os.path
 import unicodedata
 
-# OUTPUT_PATH = "%s/%s" % (get_reports.OUTPUT_DIR, get_reports.OUTPUT_FILE)
+OUTPUT_PATH = "%s/%s" % (get_reports.OUTPUT_DIR, get_reports.OUTPUT_FILE)
 
-# PWD = os.path.dirname(os.path.realpath(__file__))
-# OUTPUT_FILE = os.path.join(PWD, build_buildboard.OUTPUT_DIR_NAME,
-                            # build_buildboard.INDEX_FILE_NAME)
+def use_build_buildboard():
+    build_buildboard.build_pages_from_scratch()
+    build_buildboard.build_pages_from_existing()
 
 if __name__ == '__main__':
-    build_buildboard.build_page_from_scratch()
-    # print OUTPUT_FILE
-    # with open(OUTPUT_FILE, 'w') as outfile:
-    #     outfile.write(unicodedata.normalize('NFKD',
-    #                 build_buildboard.build_page_from_scratch()).encode('ascii','ignore')
-
-
-# if __name__ == '__main__':
-#     print OUTPUT_PATH
-#     with open(OUTPUT_PATH, 'w') as outfile:
-#         outfile.write(unicodedata.normalize('NFKD',
-#                     get_reports.create_index_page()).encode('ascii','ignore')
-# )
+    print OUTPUT_PATH
+    with open(OUTPUT_PATH, 'w') as outfile:
+        outfile.write(unicodedata.normalize('NFKD',
+                    get_reports.create_index_page()).encode('ascii','ignore')
+)
